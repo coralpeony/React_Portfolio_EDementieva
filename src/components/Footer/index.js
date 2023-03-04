c095154a4b2bb0b10bcbd37c5dd1af00c8335ba9
Coral <coralpeony21@gmail.com>
2023-03-04 20:02:37 +0200

    added Footer folder to components folder

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+  return (
+    <>
+  <div className="footer fixed-bottom">
+    <p className="footerText">Copyright &copy; 2023 CoralPeony</p>
+  </div>;
+  </>
+  )
+}
+
+export default Footer;

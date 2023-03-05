fc1a67852d020c92cea01b97815610433c820923
Coral <coralpeony21@gmail.com>
2023-03-05 20:03:57 +0200

    added styles to Contact.js file

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,10 +8,11 @@ return(
 <img className="image-container h-100" src={project.image} />
 <div className="content">
 <h5> {project.title}</h5>
-<a href={project.gitHubPages} class="btn btn-info">See the project</a>
-<a href={project.gitHub} class="btn btn-link">See the code</a>
+<a href={project.gitHubPages} className="btn btn-info">See the project</a>
+<a href={project.gitHub} className="btn btn-link">See the code</a>
 </div>
 </div>
+
 
 
 )
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,11 +4,19 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 function Contact(props) {
   return (
-    <div>
+    <div className="contactBackground">
+      <hr />
       <h1 className="textContact">Contact Me!</h1>
+      <hr />
+      <hr />
       <h3 className="textContact"> I am available for freelance work.</h3>
-<button></button>
-
+      <div class="col-md-12 text-center">
+      <a class="btn btn btn-outline-danger btn-lg margin-right" href="#" role="button">My GitHub</a>
+      <a class="btn btn btn-outline-danger btn-lg margin-right" href="#" role="button">My LinkedIn</a>
+      <a class="btn btn btn-outline-danger btn-lg margin-right" href="#" role="button">My Resume</a>
+      <a class="btn btn btn-outline-danger btn-lg margin-right" href="#" role="button">Call Me</a>
+      <a class="btn btn btn-outline-danger btn-lg margin-right" href="#" role="button">Email Me</a>
+</div>
     </div>
   );
 }
